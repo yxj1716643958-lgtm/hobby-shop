c9699e9e4051b8ea5ab2af652ea131504a812599
XIAO YUAN <yxj1716643958@gmail.com>
2025-11-29 19:50:02 +0800

    Update magazine images configuration with correct file names

--- src/lib/magazineImages.ts
+++ src/lib/magazineImages.ts
@@ -4,11 +4,11 @@
 export const magazineImages: Record<string, string[]> = {
 	// 十周年特刊
 	'1': [
-		'/magazines/10th-anniversary/封面封底-01.jpg',
-		'/magazines/10th-anniversary/目录-2P.jpg',
+		'/magazines/10th-anniversary/cover-10th.jpg',
 		'/magazines/10th-anniversary/66.jpg',
 		'/magazines/10th-anniversary/082-083-1.jpg',
 		'/magazines/10th-anniversary/086.jpg',
+		'/magazines/10th-anniversary/088.jpg',
 		'/magazines/10th-anniversary/092.jpg',
 		'/magazines/10th-anniversary/182-183-1.jpg',
 		'/magazines/10th-anniversary/201.jpg',
@@ -20,13 +20,14 @@ export const magazineImages: Record<string, string[]> = {
 		'/magazines/2024/1/002-003.jpg',
 		'/magazines/2024/1/010-011.jpg',
 		'/magazines/2024/1/014-015.jpg',
+		'/magazines/2024/1/080-081.jpg',
+		'/magazines/2024/1/110-111.jpg',
 	],
 	// 2025年1月
 	'3-01': [
 		'/magazines/2025/01/001.jpg',
-		'/magazines/2025/01/002-003 拷贝.jpg',
-		'/magazines/2025/01/004-005 拷贝.jpg',
-		'/magazines/2025/01/010-011 拷贝.jpg',
+		'/magazines/2025/01/070-071.jpg',
+		'/magazines/2025/01/074-075.jpg',
 	],
 };
 
